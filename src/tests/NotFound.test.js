6da19f863078b355c7ccdfa7f6684bb7ff9a5d29
Fernando Bueno <fernando_jordi@hotmail.com>
2022-05-09 19:38:18 -0300

    adiciona o requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,28 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from '../helpers/RenderwithRouter';
+import App from '../App';
+
+describe('Teste o componente <NotFound.js />', () => {
+  it('Teste se contém um heading h2 com o texto Page requested not found 😭', () => {
+    const { history } = renderWithRouter(<App />);
+
+    history.push('/xablau');
+
+    const notFoundEl = screen.getByRole('heading',
+      { name: /page requested not found crying emoji/i });
+
+    expect(notFoundEl).toBeInTheDocument();
+  });
+  it('', () => {
+    const { history } = renderWithRouter(<App />);
+
+    history.push('/xablau');
+
+    const imgEl = screen.getByRole('img',
+      { name: /pikachu crying because the page requested was not found/i });
+
+    expect(imgEl.src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
